a0edf5a7bca8dceaa03fa5caa1aaab05d195bf7f
David Jimenez <david.jimenez19@gmail.com>
2016-04-30 17:04:42 +0200

    init commit
    - created routes for the list and detail action

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -1,4 +1,5 @@
 export function routerConfig ($stateProvider, $urlRouterProvider) {
+  //noinspection BadExpressionStatementJS
   'ngInject';
   $stateProvider
     .state('home', {
@@ -6,6 +7,18 @@ export function routerConfig ($stateProvider, $urlRouterProvider) {
       templateUrl: 'app/main/main.html',
       controller: 'MainController',
       controllerAs: 'main'
+    })
+    .state('filmDetail', {
+      url: '/film/:id',
+      templateUrl: 'app/detail/detail.html',
+      controller: 'DetailController',
+      controllerAs: 'detail'
+    })
+    .state('filmList', {
+      url: '/list',
+      templateUrl: 'app/list/list.html',
+      controller: 'ListController',
+      controllerAs: 'list'
     });
 
   $urlRouterProvider.otherwise('/');
