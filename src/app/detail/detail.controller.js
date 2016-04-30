d67745e3d684811c20e635cd64738b2448b038e5
David Jimenez <david.jimenez19@gmail.com>
2016-05-01 00:18:35 +0200

    read data from the express server, requests to the api are redirected to the express server with the help of http-proxy-middleware module

--- src/app/detail/detail.controller.js
+++ src/app/detail/detail.controller.js
@@ -1,15 +1,32 @@
 export class DetailController {
-  constructor($scope) {
+  constructor($scope, $http, $log) {
     //noinspection BadExpressionStatementJS
     'ngInject';
 
     this.creationDate = 1462012740839;
     this.$scope = $scope;
+    this.$http = $http;
+    this.$log = $log;
 
-    this.$scope.movie = {
-      id: 1
-    };
+    this._getDetail(2);
   }
 
+  _getDetail(id) {
+
+    this.$http({
+      method: 'GET',
+      url: `http://localhost:3000/v1/film/${id}`,
+      headers: {
+        'Content-Type': 'application/JSON'
+      }
+    }).then((response) => {
+
+      this.$scope.movie = response.data;
+    }, (response) => {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+      this.$log.error(`http error: ${response.status}`);
+    });
+  }
 
 }
