9a717dec9caa1b6b7db86058475558ec59b40df0
David Jimenez <david.jimenez19@gmail.com>
2016-05-01 18:31:52 +0200

    move http call to the detail endpoint into the service

--- src/app/detail/detail.controller.js
+++ src/app/detail/detail.controller.js
@@ -1,32 +1,19 @@
 export class DetailController {
-  constructor($scope, $http, $log) {
+  constructor($scope, filmService, $log) {
     //noinspection BadExpressionStatementJS
     'ngInject';
 
     this.creationDate = 1462012740839;
     this.$scope = $scope;
-    this.$http = $http;
     this.$log = $log;
 
-    this._getDetail(2);
-  }
-
-  _getDetail(id) {
-
-    this.$http({
-      method: 'GET',
-      url: `http://localhost:3000/v1/film/${id}`,
-      headers: {
-        'Content-Type': 'application/JSON'
-      }
-    }).then((response) => {
-
-      this.$scope.movie = response.data;
-    }, (response) => {
-      // called asynchronously if an error occurs
-      // or server returns response with an error status.
-      this.$log.error(`http error: ${response.status}`);
+    this.$scope.movie = filmService.getFilm(2).then((data) => {
+      this.$scope.movie = data;
+    }).catch(() => {
+      this.$scope.error = 'unable to get the film information';
+      $log.log(`Error fetching movie name: ${this.$scope.movie.name}`);
     });
+
   }
 
 }
--- src/app/services/film/film.service.js
+++ src/app/services/film/film.service.js
@@ -1,23 +1,39 @@
 
 export class FilmService {
 
-  constructor ($http) {
+  constructor ($http, $q) {
     //noinspection BadExpressionStatementJS
     'ngInject';
 
     this.creationDate = 1462012740839;
     this.$http = $http;
+    this.$q = $q;
+
   }
 
   /**
    *
    * @param id
-   * @returns {{id: *}}
+   * @returns {Function}
      */
   getFilm(id) {
+    var defer = this.$q.defer();
 
-    return {
-      id: id
-    }
+    this.$http({
+      method: 'GET',
+      url: `http://localhost:3000/v1/film/${id}`,
+      headers: {
+        'Content-Type': 'application/JSON'
+      }
+    }).then((response) => {
+
+      defer.resolve(response.data);
+    }, (response) => {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+      defer.reject(response);
+    });
+
+    return defer.promise;
   }
 }
